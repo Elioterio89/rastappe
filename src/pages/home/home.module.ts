4c407cb580ebf5e42774c61760cf5dba379c68aa
Eslei Elioterio <eslei.elioterio@salvador.ba.gov.br>
2018-08-30 12:10:26 -0300

    alteraçoes semge navegacoes

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
+import { IonicPageModule, IonicModule } from 'ionic-angular';
 import { HomePage } from './home';
+import { EventosPage } from '../eventos/eventos';
 
 @NgModule({
   declarations: [
     HomePage,
+    EventosPage
+  ],
+  entryComponents: [
+    HomePage,
+    EventosPage
   ],
   imports: [
+    IonicModule.forRoot(HomePage),
     IonicPageModule.forChild(HomePage),
   ],
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component ,ViewChild} from '@angular/core';
+import { IonicPage, NavController, NavParams, Nav } from 'ionic-angular';
+
+
+///////////paginas/////////////
+import { EventosPage } from '../eventos/eventos';
+//////////////////////////////
 
 
 @IonicPage()
@@ -9,11 +14,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
+  pages: Array<{title: string, icon:string, component: any}>;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.pages = [
+      { title: 'Home',icon:'md-contact', component: HomePage },
+      { title: 'Eventos',icon:'md-calendar', component: EventosPage }
+    ];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+   // this.navCtrl.push(EventosPage);
+    this.navCtrl.setRoot(page.component);
   }
 
 }
