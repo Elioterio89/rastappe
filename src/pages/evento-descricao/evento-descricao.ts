9ccc2e0511860e1f0cd76f01e23448acd53df719
Eslei Elioterio <eslei.elioterio@gmail.com>
2018-08-31 23:04:47 -0300

    pagina de descricao evento

--- src/pages/evento-descricao/evento-descricao.ts
+++ src/pages/evento-descricao/evento-descricao.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, AlertController } from 'ionic-angular';
 import { CardSlide } from '../../app/classes/CardSlide ';
+import { ModalMapPage } from '../modal-map/modal-map';
 
 @IonicPage()
 @Component({
@@ -12,9 +13,38 @@ export class EventoDescricaoPage {
 
   
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public modalCtrl:ModalController,public alertCtrl: AlertController) {
      this.evento = navParams.get('evento');
      console.log(this.evento );
+  }
+
+  
+  abrirMapa() {
+    this.modalCtrl.create(ModalMapPage).present();
+  }
+
+  AlertNomelista() {
+    let prompt = this.alertCtrl.create({
+      title: "Lista Amiga",
+      message: "Envie o nome aqui!",
+      inputs: [
+        {
+          name: 'mome',
+          placeholder: 'Nome'
+        },
+      ],
+      buttons: [
+        {
+          text: 'OK',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ],
+      cssClass: 'alertLista'
+    });
+    prompt.present();
   }
 
   ionViewDidLoad() {
