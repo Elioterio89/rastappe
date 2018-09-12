acc3c34f03fabfe290d00433f96a76fcc26e18e8
Eslei Elioterio <eslei.elioterio@salvador.ba.gov.br>
2018-09-12 12:22:47 -0300

    alteraçoes viasualiz tela inicial de eventos

---
+++ src/assets/imgs/Vagner Santana - Vagner Santana compartilhou uma publicação._files/wUiPyCezoGy.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["pUxJt"]); }
+
+__d("XVideoCallOffDialogController",["XController"],(function(a,b,c,d,e,f){e.exports=b("XController").create("/videocall/off/",{__asyncDialog:{type:"Int"}})}),null);
