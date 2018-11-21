08f6c50c5739255e7d2734596b2c1ce927a956de
Eslei Elioterio <eslei.elioterio@salvador.ba.gov.br>
2018-11-21 09:51:45 -0300

    nomelistaqause

--- src/pages/nome-lista-modal/nome-lista-modal.ts
+++ src/pages/nome-lista-modal/nome-lista-modal.ts
@@ -14,12 +14,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'nome-lista-modal.html',
 })
 export class NomeListaModalPage {
+  nomes: Array<{titulo: string, type: string, value: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.nomes = [
+      { titulo: "Nome/Sobrenome", type: "text", value: "" },
+  ];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NomeListaModalPage');
   }
 
+
+  addNome(){
+    console.log(this.nomes);
+    this.nomes.push({ titulo: "Nome/Sobrenome", type: "text", value: "" });
+  }
 }
